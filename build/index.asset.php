ad846928a3c4719baedc9fdec258a182615812a7
Scottie5689 <>
2021-03-24 08:08:39 -0700

    intial commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => 'af79e9771e30770bde6d6fd4ff35ad0e');
